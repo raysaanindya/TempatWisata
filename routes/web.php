2c8fea501f9405eb1738fee8152f22986578b19b
bungarihhadatul <bungahedatul@gmail.com>
2023-11-22 11:28:01 +0700

    hello

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [AppController::class,"home"]);
+
+Route::get("hello",[AppController::class,"hello"]);
+Route::get("template",function(){ return view('template'); });
+
+Route::get("form-tambah",function(){ return view('form-tambah'); });
+Route::get("hello",[AppController::class,"profil"]);
+Route::get("hello",[AppController::class,"kontak"]);
+
+Route::get("data-wisata",[AppController::class,"data_wisata"]);
+Route::get("data-wisata/{id}/hapus",[AppController::class,"hapus_pegawai"]);
+Route::get("tambah-wisata",[AppController::class,"tambah_wisata"]);
+Route::post("tambah-wisata",[AppController::class,"proses_tambah_wisata"]);
+Route::get("data-wisata/{id}/edit",[AppController::class,"edit_wisata"]);
+Route::post("edit-wiasat",[AppController::class,"proses_edit_pegawai"]);

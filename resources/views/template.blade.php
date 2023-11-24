82f67ad0ea3fb58be86b7ba451a54f1618498280
bungarihhadatul <bungahedatul@gmail.com>
2023-11-24 13:50:24 +0700

    web

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,32 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel</title>
-    <link href="{{ url('bootstrap/css/bootstrap.min.css') }}"rel="stylesheet">
+    <title>@yield("tittle")</title>
+    <link rel="stylesheet" href="{{ url('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{url('assets/css/fontawesome.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/templatemo-villa-agency.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/owl.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/animate.css')}}">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
 </head>
 <body>
-<ul class="nav">
-        <li class="nav-item">
-            <a href="{{ url('/') }}" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ url('data-wisata') }}" class="nav-link">data</a>
-        </li>
-    </ul>
-    <hr>
+    <style>
+        
+        ul.li.a{
+            text-decoration: none;
+        }
+
+    </style>
+     <header class="header-area header-sticky">
     <div class="container">
-        <h1>@yield("title")</h1>
-        <hr>
-        @if(session()->has("pesan"))
-        <div class="alert alert-info">
-            {{ session("pesan") }}
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <h1>Wisata</h1>
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                      <li><a href="{{url('/')}}" >Home</a></li>
+                      <li><a href="{{url('data-wisata')}}" >Data Wisata</a></li>
+                      
+                      
+                  </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
         </div>
-        @endif
-        @yield("content")
     </div>
-     <hr>
-     
-  <h1>Hello</h1>
-    <script src="{{ url('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  </header>
+
+    <div class="container">
+    
+    <hr>
+    
+
+    @if(session()->has("pesan"))
+    <div class="alert alert-info">
+        {{session("pesan")}}
+    </div>
+    @endif
+
+    @yield("content")
+    </div>
+    <script src="{{ url('/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', [AppController::class,"home"]);
+Route::get('/', function () {
+    return view('home');
+});
 
-Route::get("template",function(){ return view('template'); });
-
-Route::get("profil",function(){ return view('profil');});
-Route::get("data_wisata",function(){ return view('data_wisata');});
-
+Route::get("/",[AppController::class,"home"]);
 Route::get("data-wisata",[AppController::class,"data_wisata"]);
-Route::get("data-wisata/{id}/hapus",[AppController::class,"hapus"]);
-Route::get("tambah-wisata",[AppController::class,"tambah_wisata"]);
-Route::post("tambah-wisata",[AppController::class,"proses_tambah_wisata"]);
-Route::get("data/{id}/edit",[AppController::class,"edit"]);
-Route::post("edit-wisata",[AppController::class,"proses_edit_wisata"]);
+
+Route::get('tambah-wisata', [AppController::class, 'tambah_wisata']);
+Route::post('proses-tambah-wisata', [AppController::class, 'proses_tambah_wisata'])->name('destinations.tambah');
+
+Route::get("data-wisata/{id}/hapus",[AppController::class,"hapus_wisata"]);
+
+Route::get("data-wisata/{id}/edit",[AppController::class,"edit_wisata"]);
+Route::post("edit-wisata", [AppController::class, 'proses_edit_wisata'])->name('destinations.edit');
